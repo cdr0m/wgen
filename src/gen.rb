de90aa4187d88b9fc4313f506f2197036947c1d0
ssr7 <ssr7@runbox.com>
2023-06-30 00:11:15 +0100

    Cleanup

--- src/gen.rb
+++ src/gen.rb
@@ -2,67 +2,50 @@
 $name = "Cave of Birds"
 $url = "https://caveofbirds.neocities.org"
 $source = "https://codeberg.org/ssr7/wgen"
+$input_dir = "inc"
+$output_dir = "../site"
+$links = "../links" # stylesheet, favicon, etc
 
-def parse(f)
-	c = File.read("inc/#{f}")
+def main
+	files = Dir.children("inc")
+	puts "Files found: #{files.inspect}"
 
-	# Replace embed tags with content
 	i = 0
-	embeds = c.scan(/\{[^\/].*?\}/)
-	embeds.each do |embed|
-		unless embeds.empty?
-			f_name = embed.scan(/(?<=\{\/).*?(?=\})/)
-			f_name = f_name[0].downcase
-			f_name.sub!(/\s/, "_")
-
-			content = File.read("inc/#{f}")
-			c.sub!(/#{embeds[i]}/, content)
-			i += 1
+	files.each do |file|
+		next if file.start_with?("meta.")
+		next if !file.end_with?(".html")
+		File.open("#$output_dir/#{file}", "w") do |f|
+			content = build_page(file)
+			content.each { |line| f.puts line }
+			puts "Generated: #{file}"
 		end
-	end
-
-	# Replace link tags with <a> tags
-	i = 0
-	links = c.scan(/\{.*?\}/)
-	links.each do |link|
-		f_name = link.scan(/(?<=\{).*?(?=\})/)
-		f_name = f_name[0].downcase
-		f_name.sub!(/\s/, "_")
-
-		a_link = link.sub(/^\{/, "<a href='#{f_name}.html'>")
-		a_link = a_link.sub(/\}$/, "</a>")
-
-		c.sub!(/#{links[i]}/, a_link)
 		i += 1
 	end
 
-	puts "embeds: " + embeds.inspect
-	puts "links: " + embeds.inspect
-
-	return c
+	puts "Files generated: #{i}"
 end
 
-def assemble(f)
-	nav = File.read("inc/meta.nav.html")
-	content = parse(f)
-	modified = File.mtime("inc/#{f}")
+def build_page(f)
+	nav = File.read("#$input_dir/meta.nav.html")
+	modified = File.mtime("#$input_dir/#{f}")
 	title = File.basename(f, ".html").capitalize()
+	content = parse(f)
 
 	output = [
 		"<!DOCTYPE html><html lang='en-gb'>",
 		"<head><meta charset='UTF=8'>",
 		"<title>#$name - #{title}</title>",
-		"<link href='../links/style.css' rel='stylesheet'>",
-		"<link href='../links/icon.svg' rel='icon'>",
+		"<link href='#$links/style.css' rel='stylesheet'>",
+		"<link href='#$links/icon.svg' rel='icon'>",
 		"</head><body>",
-    "<header><img src='../links/icon.svg'><span>#$name</span></header>",
+		"<header><img src='#$links/icon.svg'><span>#$name</span></header>",
 		"<div class='flex'><nav>#{nav}</nav>",
 		"<!-- Generated file -->",
 		"<main><h1>#{title}</h1>",
 		"#{content}</main></div>",
 		"<footer>",
 		"<span>ssr7 &copy; 2023</span>",
-		"<span>#{modified.strftime("Last modified: %T %Z %A, %-d %B %Y")}",
+		"<span>Last modified: #{modified.strftime("%T %Z %A, %-d %B %Y")}",
 		"[<a href='#$source/_edit/master/src/inc/#{f}' target='_blank'>edit</a>]</span>",
 		"</footer>",
 		"</body></html>"
@@ -71,22 +54,28 @@ def assemble(f)
 	return output
 end
 
-def main
-	files = Dir.children("inc")
-	puts "Files found: " + files.inspect
+def parse(f)
+	c = File.read("inc/#{f}")
 
-	i = 0
-	files.each do |file|
-		next if file.include?("meta")
-		File.open("../site/#{file}", "w") { |f|
-			content = assemble(file)
-			content.each { |line| f.puts line }
-			puts "Generated: #{file}"
-		}
-		i += 1
+	embeds = c.scan(/\{\/.*?\}/)
+	embeds.each_with_index do |embed, i|
+		f_name = embed.scan(/(?<=\{\/).*?(?=\})/)
+		f_name = f_name[0].downcase
+		f_name.sub!(/\s/, "_")
+		c.sub!(/#{embeds[i]}/, File.read("#$input_dir/#{f_name}.html"))
 	end
 
-	puts "Files generated: #{i}"
+	links = c.scan(/\{[^\/].*?\}/)
+	links.each_with_index do |link, i|
+		f_name = link.scan(/(?<=\{).*?(?=\})/)
+		f_name = f_name[0].downcase
+		f_name.sub!(/\s/, "_")
+		a_link = link.sub(/^\{/, "<a href='#{f_name}.html'>")
+		a_link = a_link.sub(/\}$/, "</a>")
+		c.sub!(/#{links[i]}/, a_link)
+	end
+
+	return c
 end
 
 main()
