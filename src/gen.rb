5ff9757e46c1a1afaf47604a422b5b661200300b
ssr7 <ssr7@runbox.com>
2023-07-03 10:59:01 +0100

    Better parsing

--- src/gen.rb
+++ src/gen.rb
@@ -7,93 +7,76 @@ $output_dir = "../site"
 $links = "../links" # stylesheet, favicon, etc
 
 def main
-	files = Dir.children("inc")
-	puts "Files found: #{files.inspect}"
+  files = Dir.children("inc")
+  puts "Files found: #{files.inspect}"
 
-	i = 0
-	files.each do |file|
-		next if file.start_with?("meta.")
-		next if !file.end_with?(".html")
-		File.open("#$output_dir/#{file}", "w") do |f|
-			content = build_page(file)
-			content.each { |line| f.puts line }
-			puts "Generated: #{file}"
-		end
-		i += 1
-	end
+  i = 0
+  files.each do |file|
+    next if file.start_with?("meta.") || !file.end_with?(".html")
+    File.open("#$output_dir/#{file}", "w") do |f|
+      content = build_page(file)
+      content.each { |line| f.puts line }
+      puts "Generated: #{file}"
+    end
+    i += 1
+  end
 
-	puts "Files generated: #{i}"
+  puts "Files generated: #{i}"
 end
 
 def build_page(f)
-	modified = File.mtime("#$input_dir/#{f}")
-	title = File.basename(f, ".html").capitalize()
-	nav = parse("meta.nav.html")
-	content = parse(f)
+  modified = File.mtime("#$input_dir/#{f}")
+  title = File.basename(f, ".html").capitalize()
+  nav = parse("meta.nav.html")
+  content = parse(f)
 
-	if f == "index.html"
-		files = Dir.children("inc").sort
-		content = "<ul>"
-		files.each do |file|
-			content += "<li><a href='#{file}' class='cap'>#{file.split(/\s|\./)[0]}</a></li>\n"
-		end
-		content += "</ul>"
-	end
+  if f == "index.html"
+    files = Dir.children("inc").sort
+    content = "<ul>"
+    files.each do |file|
+      content += "<li><a href='#{file}' class='cap'>#{file.split(/\s|\./)[0]}</a></li>\n"
+    end
+    content += "</ul>"
+  end
 
-	output = [
-		"<!DOCTYPE html><html lang='en-gb'>",
-		"<head><meta charset='UTF=8'>",
-		"<title>#$name - #{title}</title>",
-		"<link href='#$links/style.css' rel='stylesheet'>",
-		"<link href='#$links/icon.svg' rel='icon'>",
-		"</head><body>",
-		"<header><img src='#$links/icon.svg'><span>#$name</span></header>",
-		"<div class='flex'><nav>#{nav}</nav>",
-		"<!-- Generated file -->",
-		"<main><h1>#{title}</h1>",
-		"#{content}</main></div>",
-		"<footer>",
-		"<span>ssr7 &copy; 2023</span>",
-		"<span>Last modified: #{modified.strftime("%T %Z %A, %-d %B %Y")}",
-		"<a href='#$source/_edit/master/src/inc/#{f}' target='_blank'>edit</a>]</span>",
-		"</footer>",
-		"</body></html>"
-	]
+  output = [
+    "<!DOCTYPE html><html lang='en-gb'>",
+    "<head><meta charset='UTF=8'>",
+    "<title>#$name - #{title}</title>",
+    "<link href='#$links/style.css' rel='stylesheet'>",
+    "<link href='#$links/icon.svg' rel='icon'>",
+    "</head><body>",
+    "<header><img src='#$links/icon.svg'><span>#$name</span></header>",
+    "<div class='flex'><nav>#{nav}</nav>",
+    "<!-- Generated file -->",
+    "<main><h1>#{title}</h1>",
+    "#{content}</main></div>",
+    "<footer>",
+    "<span>ssr7 &copy; 2023</span>",
+    "<span>Last modified: #{modified.strftime("%T %Z %A, %-d %B %Y")}",
+    "<a href='#$source/_edit/master/src/inc/#{f}' target='_blank'>edit</a>]</span>",
+    "</footer>",
+    "</body></html>"
+  ]
 
-	return output
+  return output
 end
 
 def parse(f)
-	content = File.read("inc/#{f}")
+  content = File.read("inc/#{f}")
+  tags = content.scan(/\{\/?[^\/].*?\}/)
+  tags.each do |tag|
+    filename = tag.scan(/(?<=\{).*?(?=\})/)
+    filename = filename[0].sub(/\s/, "_")
 
-	embeds = content.scan(/\{\/.*?\}/)
-	embeds.each_with_index do |embed, i|
-    f_name = filename(embed, "embed")
-		content.sub!(/#{embeds[i]}/, File.read("#$input_dir/#{f_name}.html"))
-	end
-
-	links = content.scan(/\{[^\/].*?\}/)
-	links.each_with_index do |link, i|
-    f_name = filename(link, "link")
-		a_link = link.sub(/^\{/, "<a href='#{f_name}.html'>")
-		a_link = a_link.sub(/\}$/, "</a>")
-		content.sub!(/#{links[i]}/, a_link)
-	end
-
-	return content
-end
-
-def filename(string, action)
-  if action == "embed"
-		f_name = string.scan(/(?<=\{\/).*?(?=\})/)
+    if tag.match?(/\{\/.*?\}/)
+      content.sub!(/#{tag}/, File.read("#$input_dir/#{filename.downcase}.html"))
+    else
+      a_tag = tag.sub(/^\{/, "<a href='#{filename.downcase}.html'>")
+      a_tag = a_tag.sub(/\}$/, "</a>")
+      content.sub!(/#{tag}/, a_tag)
+    end
   end
-
-  if action == "link"
-		f_name = string.scan(/(?<=\{).*?(?=\})/)
-  end
-
-  content = f_name[0].downcase
-  content.sub!(/\s/, "_")
 
   return content
 end
