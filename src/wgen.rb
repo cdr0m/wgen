dc565d073bc5d6e680efd91087423097e537dac0
ssr7 <ssr7@runbox.com>
2023-07-14 13:52:22 +0100

    Report missing files when embedding

--- src/wgen.rb
+++ src/wgen.rb
@@ -11,69 +11,80 @@ def parse(f)
   tags = content.scan(/\{\/?[^\/].*?\}/)
   tags.each do |tag|
     filename = tag.tr("{/}", "")
-    filename.gsub!(/\s/, "_")    
+    filename.gsub!(/\s/, "_")
 
     if tag.match?(/\{\/.*?\}/)
-      content.sub!(/#{tag}/, "<h2 class='cap'><a href='#{filename}'>#{filename}</a></h2>" +
-                             File.read("#$input_dir/#{filename.downcase}.html"))
+      if File.exists?("#$input_dir/#{filename.downcase}.html")
+        content.sub!(/#{tag}/,
+          "<h2 class='cap'><a href='#{filename.downcase}'>#{filename}</a></h2>" +
+          File.read("#$input_dir/#{filename.downcase}.html"))
+      else
+        puts "Missing file: #{filename.downcase}.html embedded on #{f}"
+        content.sub!(/#{tag}/, "<div class='error'>Missing file: #{filename.downcase}.html</div>")
+      end
     else
       a_tag = tag.sub(/^\{/, "<a href='#{filename.downcase}.html'>")
       a_tag.sub!(/\}$/, "</a>")
       content.sub!(/#{tag}/, a_tag)
     end
   end
-  
+
   return content
 end
 
-files = Dir.children("inc").sort
-files.keep_if {|file| file =~ /^[^.]+\.html$/}
+def build()
+  files = Dir.children("inc").sort
+  files.keep_if {|file| file =~ /^[^.]+\.html$/}
 
-if files.empty?
-  abort("No files found in .\/#$input_dir")
-end
-
-puts "Files found: #{files.inspect}"
-
-i = 0
-files.each do |file|
-  File.open("#$output_dir/#{file}", "w") do |f|
-    modified = File.mtime("#$input_dir/#{file}")
-    title = File.basename(file, ".html").capitalize()
-    nav = parse("meta.nav.html")
-    content = parse(file)
-
-    if file == "index.html"
-      content = "<ul>"
-      files.each do |file|
-        content += "<li><a href='#{file}' class='cap'>#{file.split(/\s|\./)[0]}</a></li>\n"
-      end
-      content += "</ul>"
-    end
-
-    f.puts "<!DOCTYPE html><html lang='en-gb'>"
-    f.puts "<head><meta charset='UTF=8'>"
-    f.puts "<title>#$name - #{title}</title>"
-    f.puts "<link href='#$links/style.css' rel='stylesheet'>"
-    f.puts "<link href='#$links/icon.svg' rel='icon'>"
-    f.puts "</head><body>"
-    f.puts "<header><a href='home.html'><img src='#$links/icon.svg'><span>#$name</span></a></header>"
-    f.puts "<div class='flex'><nav>#{nav}</nav>"
-    f.puts "<!-- Generated file -->"
-    f.puts "<main><h1>#{title}</h1>"
-    f.puts "#{content}</main></div>"
-    f.puts "<footer>"
-    f.puts "<span>ssr7 &copy; 2023</span>"
-    f.puts "<span>Last modified: #{modified.strftime("%T %Z %A, %-d %B %Y")}"
-    f.puts "<a href='#$source/_edit/master/src/inc/#{file}' target='_blank'>[edit]</a></span>"
-    f.puts "</footer>"
-    f.puts "</body></html>"
-    f.close
-    puts "Generated: #{file}"
+  if files.empty?
+    abort("No files found in .\/#$input_dir")
   end
 
-  i += 1
+  puts "Files found: #{files.inspect}"
+
+  i = 0
+  files.each do |file|
+    File.open("#$output_dir/#{file}", "w") do |f|
+      modified = File.mtime("#$input_dir/#{file}")
+      title = File.basename(file, ".html").capitalize()
+      nav = parse("meta.nav.html")
+      content = parse(file)
+
+      if file == "index.html"
+        content = "<ul>"
+        files.each do |file|
+          content += "<li><a href='#{file}' class='cap'>#{file.split(/\s|\./)[0]}</a></li>\n"
+        end
+        content += "</ul>"
+      end
+
+      f.puts "<!DOCTYPE html><html lang='en-gb'>"
+      f.puts "<head><meta charset='UTF=8'>"
+      f.puts "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+      f.puts "<title>#$name - #{title}</title>"
+      f.puts "<link href='#$links/style.css' rel='stylesheet'>"
+      f.puts "<link href='#$links/icon.svg' rel='icon'>"
+      f.puts "</head><body>"
+      f.puts "<header><a href='home.html'><img src='#$links/icon.svg'><span>#$name</span></a></header>"
+      f.puts "<div class='flex'><nav>#{nav}</nav>"
+      f.puts "<!-- Generated file -->"
+      f.puts "<main><h1>#{title}</h1>"
+      f.puts "#{content}"
+      f.puts "</main></div>"
+      f.puts "<footer>"
+      f.puts "<span>ssr7 &copy; 2023</span>"
+      f.puts "<span>Last modified: #{modified.strftime("%T %Z %A, %-d %B %Y")}"
+      f.puts "<a href='#$source/_edit/master/src/inc/#{file}' target='_blank'>[edit]</a></span>"
+      f.puts "</footer>"
+      f.puts "</body></html>"
+      f.close
+      puts "Generated: #{file}"
+    end
+
+    i += 1
+  end
+
+  puts "#{i} files generated"
 end
 
-puts "#{i} files generated"
-
+build()
